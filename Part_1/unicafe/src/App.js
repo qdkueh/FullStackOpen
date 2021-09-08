6d1d71c0f89309d22081403d31787ff967fb0c3d
PickledBrains <cindykueh@gmail.com>
2021-09-08 16:37:48 +0800

    Exercise 1.6-1.14

--- Part_1/unicafe/src/App.js
+++ Part_1/unicafe/src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const StatisticsLine = ({text, value}) => {
+  if(text==="Review") {
+    return (
+      <tr>
+        <th>{text}</th>
+        <th>{value}</th>
+      </tr>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
 }
 
-export default App;
+const Statistics = ({good, neutral, bad, all}) => {
+    if(all.length === 0) {
+      return (
+        <p>No Feedback received yet.</p>
+      )
+    }
+    return (
+      <>
+        <table>
+          <StatisticsLine text="Review" value="Count"/>
+          <StatisticsLine text="Good" value={good}/>
+          <StatisticsLine text="Neutral" value={neutral}/>
+          <StatisticsLine text="Bad" value={bad}/>
+          <StatisticsLine text="All" value={all.length}/>
+          <StatisticsLine text="Average" value={all.reduce((a, b)=> a + b, 0)/all.length}/>
+          <StatisticsLine text="Positive" value={(good/(all.length) * 100) +"%" }/>
+        </table>
+
+      </>
+    )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState([])
+  
+  const goodClick = () => {
+    setAll(all.concat(1))
+    setGood(good + 1)
+  }
+  const neutralClick = () => {
+    setAll(all.concat(0))
+    setNeutral(neutral + 1)
+  }
+  const badClick = () => {
+    setAll(all.concat(-1))
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+        <Button handleClick={goodClick} text='Good' />
+        <Button handleClick={neutralClick} text='Neutral' />
+        <Button handleClick={badClick} text='Bad' />
+      <h1>Statistics</h1>
+        <Statistics good = {good} neutral = {neutral} bad = {bad} all = {all}/>
+    </div>
+  )
+}
+
+export default App
